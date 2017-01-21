fab9413eea19f054ad99360621b2b57aff8153cc
Dylan Dumesnil <dadumesnil@gmail.com>
2017-01-21 13:24:48 -0800

    Body moves instead of tear, tear to body collision detection

--- Tear.gd
+++ Tear.gd
@@ -4,16 +4,15 @@ extends Sprite
 # var a = 2
 # var b = "textvar"
 
-export(Vector2) var TearSpeed = Vector2(0,0)
+export(NodePath) var Collider = "Area2D/CollisionPolygon2D"
 export(int) var WaveStrength = 5
 
+var AreaCollider
+
 func _ready():
-	set_process(true)
+	AreaCollider = get_node(Collider)
 	set_process_input(true)
 
-func _process(delta):
-	set_pos(get_pos() + TearSpeed * delta)
-	
 func _input(event):
 	if(event.is_action_pressed("wave_tear_right")):
 		set_pos(get_pos() + Vector2(WaveStrength, 0))
