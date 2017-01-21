2dd2e005dbb9358cd80b646065e504c1fdfd8b33
Hamza Faran <hafaran@microsoft.com>
2017-01-21 14:32:59 -0800

    Delete child camera; resize

--- GameRoot.gd
+++ GameRoot.gd
@@ -8,19 +8,8 @@ const numPlayers = 1
 var screen_size = Vector2(0,0)
 var player_emitters = []
 
-func handle_screen_resize():
-	#screen_size = get_viewport_rect().size
-	screen_size = OS.get_window_size()
-	print("Handling screen resize...")
-	for emitters in player_emitters:
-		var right = emitters[0]
-		var left = emitters[1]
-		right.set_pos(Vector2(screen_size.x*(0.9), screen_size.y/2))
-		left.set_pos(Vector2(screen_size.x*(0.1), screen_size.y/2))
-
 func _ready():
-	#screen_size = get_viewport_rect().size
-	screen_size = OS.get_window_size()
+	screen_size = get_viewport_rect().size
 	get_tree().connect("screen_resized", self, "handle_screen_resize")
 
 	# Load initial players
@@ -29,15 +18,24 @@ func _ready():
 		var waveEmNode_Right = waveEm.instance()
 		add_child(waveEmNode_Right)
 		waveEmNode_Right.set_pos(Vector2(screen_size.x*(0.9), screen_size.y/2))
-		
+
 		var waveEmNode_Left = waveEm.instance()
 		add_child(waveEmNode_Left)
 		waveEmNode_Left.set_pos(Vector2(screen_size.x*(0.1), screen_size.y/2))
-		
+
 		var emitters = []
 		emitters.append(waveEmNode_Right)
 		emitters.append(waveEmNode_Left)
 		player_emitters.append(emitters)
+
+func handle_screen_resize():
+	screen_size = get_viewport_rect().size
+	print("Handling screen resize...")
+	for emitters in player_emitters:
+		var right = emitters[0]
+		var left = emitters[1]
+		right.set_pos(Vector2(screen_size.x*(0.9), screen_size.y/2))
+		left.set_pos(Vector2(screen_size.x*(0.1), screen_size.y/2))
 
 func OnEnterBody(area):
 	#print(area.get_parent().get_name())
