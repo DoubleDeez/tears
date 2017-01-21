928b60de4146bd5d7e38674df6bf08f6ef99960f
Dylan Dumesnil <dadumesnil@gmail.com>
2017-01-20 20:49:01 -0800

    Added input for pushing tear left/right

--- Tear.gd
+++ Tear.gd
@@ -4,8 +4,18 @@ extends Sprite
 # var a = 2
 # var b = "textvar"
 
+export(Vector2) var TearSpeed = Vector2(0,0)
+export(int) var WaveStrength = 5
+
 func _ready():
 	set_process(true)
+	set_process_input(true)
 
 func _process(delta):
-	set_pos(get_pos() + Vector2(0, 5))
+	set_pos(get_pos() + TearSpeed * delta)
+	
+func _input(event):
+	if(event.is_action_pressed("wave_tear_right")):
+		set_pos(get_pos() + Vector2(WaveStrength, 0))
+	elif(event.is_action_pressed("wave_tear_left")):
+		set_pos(get_pos() - Vector2(WaveStrength, 0))
