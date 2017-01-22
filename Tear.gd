ae2014f1da199420a3f0add975bf72a48db8c0a4
Hamza Faran <hafaran@microsoft.com>
2017-01-21 22:19:03 -0800

    Make wave more powerful

--- Tear.gd
+++ Tear.gd
@@ -5,11 +5,11 @@ extends Sprite
 # var b = "textvar"
 
 export(NodePath) var Collider = "Area2D/CollisionPolygon2D"
-export(int) var WaveStrength = 200.0
 export(Vector2) var TearScale = Vector2(0.5, 0.5)
 export(float) var ScaleMultiplier = 1.25
 export(Vector2) var MinScale = Vector2(0.25, 0.25)
 
+const wave_strength = 500.0
 
 var AreaCollider
 
@@ -26,4 +26,4 @@ func OnHitGrease():
 	get_parent().get_node("Body").OnHitGrease()
 
 func OnHitWave(wave_force, direction):
-	self.set_pos(self.get_pos() + Vector2(wave_force * WaveStrength * direction, 0))
+	self.set_pos(self.get_pos() + Vector2(wave_force * wave_strength * direction, 0))
