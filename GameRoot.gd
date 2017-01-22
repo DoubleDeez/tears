b8d4ea7c4032bc2aff49cd56480abc254635fc0f
Hamza Faran <hafaran@microsoft.com>
2017-01-21 20:01:06 -0800

    Use proper camera-relative placement for emitters and write util funcs for exit checks

--- GameRoot.gd
+++ GameRoot.gd
@@ -4,7 +4,6 @@ const Constants = preload("Constants.gd")
 const Utils = preload("Utils.gd")
 
 const numPlayers = 1
-const screen_x_offset_factor = 0.293
 
 export(NodePath) var BodyPath = "Body"
 
@@ -19,39 +18,30 @@ func _ready():
 	body = get_node(BodyPath)
 
 	load_emitter_players()
-
+	handle_screen_resize()
 
 func handle_screen_resize():
 	screen_size = Utils.get_viewport_size(self)
-	var screen_x_offset = -screen_size.x*(screen_x_offset_factor)
 	print("Screen size is %d x %d" % [screen_size.x, screen_size.y])
 	for emitters in player_emitters:
 		var right = emitters[0]
 		var left = emitters[1]
-		var right_vector = Vector2(screen_size.x*(0.95)+screen_x_offset, screen_size.y/2)
-		print("Setting position of right trigger... %d x %d" % [right_vector.x, right_vector.y])
-		right.set_global_pos(right_vector)
-		var left_vector = Vector2(screen_size.x*(0.05)+screen_x_offset, screen_size.y/2)
-		print("Setting position of left trigger... %d x %d" % [left_vector.x, left_vector.y])
-		left.set_global_pos(left_vector)
-
+		Utils.place_on_screen(right, 0.95, 0.5)
+		Utils.place_on_screen(left, 0.05, 0.5)
 
 func load_emitter_players():
 	# Load initial players
 	var waveEm = load(Constants.SCENE_WAVEEMITTER)
 	var RT_texture = load(Constants.TEXTURE_RIGHTEMITTER)
 	var LT_texture = load(Constants.TEXTURE_LEFTEMITTER)
-	var screen_x_offset = -screen_size.x*(screen_x_offset_factor)
 	for i in range(numPlayers):
 		var waveEmNode_Right = waveEm.instance()
 		add_child(waveEmNode_Right)
-		waveEmNode_Right.set_global_pos(Vector2(screen_size.x*(0.95)+screen_x_offset, screen_size.y/2))
 		waveEmNode_Right.set_texture(RT_texture)
 		waveEmNode_Right.set("side", "R")
 
 		var waveEmNode_Left = waveEm.instance()
 		add_child(waveEmNode_Left)
-		waveEmNode_Left.set_global_pos(Vector2(screen_size.x*(0.05)+screen_x_offset, screen_size.y/2))
 		waveEmNode_Left.set_texture(LT_texture)
 		waveEmNode_Left.set("side", "L")
 
--- Utils.gd
+++ Utils.gd
@@ -1,4 +1,38 @@
-static func get_viewport_size(node):
+static func get_root(node):
 	var root = node.get_tree().get_root().get_node("GameRoot")
-	var screen_size = root.get_viewport_rect().size * 7
-	return screen_size
+	return root
+
+static func get_camera(node, root=null):
+	if root == null:
+		root = get_root(node)
+	var camera = root.get_node("Camera2D")
+	return camera
+
+static func get_viewport_size(node, root=null):
+	if root == null:
+		root = get_root(node)
+	var camera = root.get_node("Camera2D")
+	var camera_zoom = camera.get_zoom()
+	var screen_res = root.get_viewport_rect().size
+	return Vector2(screen_res.x*camera_zoom.x, screen_res.y*camera_zoom.y)
+
+static func place_on_screen(node, x_pct, y_pct, viewport_size=null):
+	if viewport_size == null:
+		viewport_size = get_viewport_size(node)
+	var camera = get_camera(node)
+	var center = camera.get_camera_screen_center()
+	x_pct -= 0.5
+	y_pct -= 0.5
+	var pos = Vector2(viewport_size.x*(x_pct), viewport_size.y*(y_pct))  + center
+	node.set_pos(pos)
+
+static func get_camera_rect_dict(node):
+	var camera = get_camera(node)
+	var center = camera.get_camera_screen_center()
+	var viewport_size = get_viewport_size(node)
+	return {
+		"top": center.y + viewport_size.y/2,
+		"bottom": center.y - viewport_size.y/2,
+		"left": center.x + viewport_size.x/2,
+		"right": center.x - viewport_size.x/2
+	}
