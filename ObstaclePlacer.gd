46b4d3dceafc27a3f83679124c0a110cd265836b
Dylan Dumesnil <dadumesnil@gmail.com>
2017-01-21 17:10:27 -0800

    Obstacles can be placed

---
+++ ObstaclePlacer.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var BlackHead = preload("res://obstacles/BlackHead.tscn")
+var Mole = preload("res://obstacles/Mole.tscn")
+var Grease = preload("res://obstacles/Grease.tscn")
+var Pimple = preload("res://obstacles/Pimple.tscn")
+
+export(int) var Speed = 100
+
+var ObstacleArray = []
+var area
+
+func _ready():
+	area = get_node("Area2D")
+	ObstacleArray.append(BlackHead)
+	ObstacleArray.append(Mole)
+	ObstacleArray.append(Grease)
+	ObstacleArray.append(Pimple)
+	set_process_input(true)
+	set_process(true)
+	
+func _input(event):
+	if event.is_action_pressed("p2_place_obstacle"):
+		if(area.overlaps_area(get_parent().body.get_node("Area2D"))):
+			var obstacle = ObstacleArray[randi()%ObstacleArray.size()].instance()
+			get_parent().PlaceObstacle(obstacle)
+			obstacle.set_global_pos(get_global_pos())
+
+func _process(delta):
+	if abs(Input.get_joy_axis(1, 0)) > 0.1:
+		set_pos(get_pos() + Vector2(Speed*Input.get_joy_axis(1, 0), 0))
