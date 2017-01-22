1390c1fa8f5213c7351beb77ceec609d636709bd
Hamza Faran <hafaran@microsoft.com>
2017-01-21 20:55:21 -0800

    Add fading to waves

--- WaveEmitter.gd
+++ WaveEmitter.gd
@@ -4,8 +4,8 @@ const Constants = preload("Constants.gd")
 const Utils = preload("Utils.gd")
 
 var wave_scene = load(Constants.SCENE_WAVE)
-const movement_speed = 30
-const wave_movement_speed = 20
+const movement_speed = 3000
+const wave_movement_speed = 2000
 
 # Set this when creating new instance in GameRoot; MUST be one of "L" or "R"
 var side
@@ -41,11 +41,11 @@ func _process(delta):
 		js_axis = 3
 
 	if(Input.is_action_pressed(up_event)):
-		self.set_pos(self.get_pos() - Vector2(0,movement_speed))
+		self.set_pos(self.get_pos() - Vector2(0,movement_speed*delta))
 	elif(Input.is_action_pressed(down_event)):
-		self.set_pos(self.get_pos() + Vector2(0,movement_speed))
+		self.set_pos(self.get_pos() + Vector2(0,movement_speed*delta))
 	if abs(Input.get_joy_axis(0, js_axis)) > 0.1:
-		set_pos(get_pos() + Vector2(0, movement_speed*Input.get_joy_axis(0, js_axis)))
+		set_pos(get_pos() + Vector2(0, delta*movement_speed*Input.get_joy_axis(0, js_axis)))
 
 func create_wave():
 	var instance = wave_scene.instance()
