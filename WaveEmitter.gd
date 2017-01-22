33671ca1ccff49fb11683b696d4a32121d0d83e6
Hamza Faran <hafaran@microsoft.com>
2017-01-21 16:58:47 -0800

    Better position; player can now move by holding down

--- WaveEmitter.gd
+++ WaveEmitter.gd
@@ -4,38 +4,46 @@ const Constants = preload("Constants.gd")
 const Utils = preload("Utils.gd")
 
 var wave_scene = load(Constants.SCENE_WAVE)
+const movement_speed = 30
 
 # Set this when creating new instance in GameRoot; MUST be one of "L" or "R"
 var side
+
 var root
 
 func _ready():
 	root = get_tree().get_root().get_node("GameRoot")
 	set_process_input(true)
+	set_process(true)
 
 func _input(event):
+	var wave_create_event
+	if side == "L":
+		wave_create_event = "p1_lt"
+	elif side == "R":
+		wave_create_event = "p1_rt"
+
+	if(event.is_action_pressed(wave_create_event)):
+		create_wave()
+
+func _process(delta):
 	var up_event
 	var down_event
-	var wave_create_event
 	if side == "L":
 		up_event = "p1_left_emitter_up"
 		down_event =  "p1_left_emitter_down"
-		wave_create_event = "p1_lt"
 	elif side == "R":
 		up_event = "p1_right_emitter_up"
 		down_event =  "p1_right_emitter_down"
-		wave_create_event = "p1_rt"
 
-	if(event.is_action_pressed(up_event)):
-		self.set_pos(self.get_pos() - Vector2(0,10))
-	elif(event.is_action_pressed(down_event)):
-		self.set_pos(self.get_pos() + Vector2(0,10))
-	elif(event.is_action_pressed(wave_create_event)):
-		create_wave()
+	if(Input.is_action_pressed(up_event)):
+		self.set_pos(self.get_pos() - Vector2(0,movement_speed))
+	elif(Input.is_action_pressed(down_event)):
+		self.set_pos(self.get_pos() + Vector2(0,movement_speed))
 
 func create_wave():
 	var instance = wave_scene.instance()
 	instance.set("travel_speed", 10.0)
 	instance.set("direction", 1 if side == "L" else -1)
-	#root.add_child(instance)
-	add_child(instance)
+	instance.set_global_pos(self.get_global_pos())
+	root.add_child(instance)
